6e76f0bc44b81778307e498210eaf266dfce9391
tonyfant <entifantin@gmail.com>
2025-09-06 15:37:50 +0200

    Initial Commit

---
+++ PhotoCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoCleaner
+//
+//  Created by Anthony Fantin on 06/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoCleaner/PhotoCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerApp.swift
+//  PhotoCleaner
+//
+//  Created by Anthony Fantin on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
